d1d18b8f4cddcc614f2260c6db71c3f3cafe5af3
agustin lerej <aguslerej@gmail.com>
2021-09-11 19:11:20 -0300

    Filtrado

--- proyecto-integrador-react/src/Componentes/Nav/Nav.js
+++ proyecto-integrador-react/src/Componentes/Nav/Nav.js
@@ -1,6 +1,6 @@
 import React, {Component} from 'react';
 import './Nav.css';
-
+import Filtrado from '../Filtrado/Filtrado'
 
 class Navbar extends Component{
     constructor(){
@@ -18,10 +18,7 @@ class Navbar extends Component{
                             <p>Ordenar ASC/ DESC</p>
                             <i className="fas fa-th"></i>
                             <i className="fas fa-align-justify"></i>
-                            <form action="">
-                                <input type="text" name="search" id="" placeholder="Search"/>
-                                <button type="submit"><i className="fas fa-search"></i></button>
-                            </form>
+                         
                         </section>
                 </header>
             </React.Fragment>
