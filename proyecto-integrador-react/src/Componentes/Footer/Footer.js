1a6baed10a940f0e8d24049a77f01572849de773
gocampos1 <63106322+gocampos1@users.noreply.github.com>
2021-09-11 17:22:30 -0300

    footer

---
+++ proyecto-integrador-react/src/Componentes/Footer/Footer.js
@@ -0,0 +1,18 @@
+import React from "react";
+import react from "react";
+import './Footer.css';
+
+function Footer(){
+    return(
+        <React.Fragment>
+            <footer>
+                <ul className="team bg-success">
+                    <li>Agustín Lerej</li>
+                    <li>Nicolás Serena Olivera</li>
+                    <li>Gonzalo Ocampos</li>
+                </ul>
+            </footer>      
+        </React.Fragment>
+    )
+}
+export default Footer;
