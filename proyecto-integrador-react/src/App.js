48a479ffbe6d7c033ba93224421caa92ae481672
nicoserena <nicoserena90@gmail.com>
2021-09-11 17:15:25 -0300

    nav

--- proyecto-integrador-react/src/App.js
+++ proyecto-integrador-react/src/App.js
@@ -1,11 +1,12 @@
 import './App.css';
 import Musica from './Componentes/Musica/Musica'
+import Navbar from './Componentes/Nav/Nav';
 
 function App() {
   return (
     <div className="App">
       
-       
+        <Navbar/>
          <Musica />
       
       
--- proyecto-integrador-react/src/Componentes/Nav/Nav.js
+++ proyecto-integrador-react/src/Componentes/Nav/Nav.js
@@ -1,36 +1,31 @@
-import React, {Component} from 'react' 
+import React, {Component} from 'react';
+import './Nav.css';
 
-class Nav extends Component{
-    constructor(props){
-        super(props)
-        this.state = {
+class Navbar extends Component{
+    constructor(){
+        super();
+        this.state ={
 
         }
     }
+    render(){
+        return(
+            <React.Fragment>
+                <header className='encabezado bg-success text-white p-2 '>
+                        <h1>Aplicación de musica</h1>
+                        <section className='opciones'>
+                            <p>Ordenar ASC/ DESC</p>
+                            <i className="fas fa-th"></i>
+                            <i className="fas fa-align-justify"></i>
+                            <form action="">
+                                <input type="text" name="search" id="" placeholder="Search"/>
+                                <button type="submit"><i className="fas fa-search"></i></button>
+                            </form>
+                        </section>
+                </header>
+            </React.Fragment>
+        )
+    }
 
-render(){
-    return(
-
-        <React.Fragment> 
-            <header>
-                <h1>Título/ Nombre de la app</h1>
-                    <section>
-                        <p>Ordenar ASC/ DESC</p>
-                        <i class="fas fa-th"></i>
-                        <i class="fas fa-align-justify"></i>
-                        <form action="">
-                            <input type="text" name="search" id="" placeholder="Search">
-                            <button type="submit"><i class="fas fa-search"></i></button>
-                        </form>
-                    </section>
-            </header>
-
-        </React.Fragment>
-)
-} 
 }
-
-
-
-
-export default
+export default Navbar;
