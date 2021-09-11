5f14e97ea8ff6a1e3035f059f686b0cf356b0a7e
agustin lerej <aguslerej@gmail.com>
2021-09-11 17:45:14 -0300

    Ver mas, ver menos

--- proyecto-integrador-react/src/Componentes/Tarjetas/Tarjetas.js
+++ proyecto-integrador-react/src/Componentes/Tarjetas/Tarjetas.js
@@ -1,13 +1,29 @@
 import React, {Component} from 'react';
+import './Tarjetas.css'
 
 class Tarjetas extends Component{
     constructor(props){
         super(props)
         this.state = {
-
+            viewMore: false,
+            text:'Ver más'
         }
+    
     }
 
+    viewMore(){
+        if(this.state.viewMore){
+            this.setState({
+                viewMore: false,
+                text: 'Ver más'
+            })
+        } else {
+            this.setState({
+                viewMore: true,
+                text: 'ver menos'
+            })            
+        }
+    }
     render(){
         return(
             <React.Fragment>
@@ -26,11 +42,15 @@ class Tarjetas extends Component{
                             <h3>{this.props.dataMusica.title}</h3>
                             <p class="description">Esta es una cancion de {this.props.dataMusica.artist.name} </p>
                             <section class="aditional-info">
-                                <p>Album: {this.props.dataMusica.album.title}</p>
+                                
+                            </section>
+                            <p className={` ${this.state.viewMore ? 'show' : 'hide'}`}>
+                            <p>Album: {this.props.dataMusica.album.title}</p>
                                 <p>Esta cancion tiene una duracion de: {this.props.dataMusica.duration} segundos</p>
                                 <p>Se encuentra en el numero {this.props.dataMusica.position} de las mas escuchadas</p>
-                            </section>
-                            <a href="">Ver más</a>
+                            </p>
+                            <button className='mas' onClick={()=>this.viewMore()}>{this.state.text}</button>
+                            
                         </main>
                     </article>
 
